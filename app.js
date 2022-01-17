5cc316594671d0b3e3b8a6ea7655ece1d9e6faa4
kirik <alkirichenko81@gmail.com>
2022-01-17 22:25:07 +0200

    pre deploy changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -10,7 +11,14 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/collections', require('./routes/collections.routes'))
 
-const PORT = config.get('port') || 5000
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
+const PORT = config.get('port') || 80
 
 const start = async () => {
     try {
